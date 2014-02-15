c0417778be909bfea9e04892006dfbb015ff9796
wangchenggang <blueskyjerry@163.com>
2014-02-15 11:29:47 +0800

    Create url.js

---
+++ url.js
@@ -0,0 +1,8 @@
+
+function getQueryString(name) {
+    var result = location.search.match(new RegExp("[\?\&]" + name + "=([^\&]+)", "i"));
+    if (result == null || result.length < 1) {
+        return "";
+    }
+    return result[1];
+}
